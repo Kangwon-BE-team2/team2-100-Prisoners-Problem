6ae2da31f09e58c300d51bf7f0e7c45e22764738
Awhn <ahnshwan@outlook.com>
2024-06-04 20:46:56 +0900

    feat : add find_number function

--- 100_Prisoners_Problem.py
+++ 100_Prisoners_Problem.py
@@ -1,11 +1,21 @@
+import random
+
 numberList = []
 
 def main():
   
 def game():
   
-def find_number():
-
+def find_number(answer):
+    result  = False # False mean prisoner is not able to find his number
+    findList = range(100)
+    findList.shuffle()
+    findList = findList[:50]
+    for i in findList:
+        if answer == numberList[i]:
+            result = True # True mean prisoner is able to find his number
+            break
+    return result
 
 if __name__ == "__main__":
     main()
