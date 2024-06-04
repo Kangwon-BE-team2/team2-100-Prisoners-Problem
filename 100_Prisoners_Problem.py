8702c4ce03bcdf65a9e378ce3b4258a6748e3038
5win <woozzoo7@gmail.com>
2024-06-04 20:44:32 +0900

    feat: Add setRandomNumber() function - 상자에 죄수 번호를 저장하는 함수

--- 100_Prisoners_Problem.py
+++ 100_Prisoners_Problem.py
@@ -1,11 +1,17 @@
+import random
+
 numberList = []
+
+def setRandomNumber():
+    for i in range(1, 101):
+        numberList.append(i)
+    random.shuffle(numberList)
 
 def main():
   
 def game():
   
 def find_number():
-
 
 if __name__ == "__main__":
     main()
