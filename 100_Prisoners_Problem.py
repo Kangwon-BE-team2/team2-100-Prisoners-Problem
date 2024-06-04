e58eaa5685aa09524e5e308e5957f40b4d7dc134
ez23re <150822430+ez23re@users.noreply.github.com>
2024-06-04 20:41:39 +0900

    feat: game() 함수 작성

--- 100_Prisoners_Problem.py
+++ 100_Prisoners_Problem.py
@@ -3,8 +3,15 @@ numberList = []
 def main():
   
 def game():
-  
+  setRandomNumber()
+    for prisoner in range(100):
+        if not find_number(prisoner):
+            return False
+    return True
+
 def find_number():
+
+def setRandomNumber(): 
 
 
 if __name__ == "__main__":
